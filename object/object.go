051dfa195dd2f7916fb5bfc2ecf630cb86c22909
Solomon <914573285@qq.com>
2019-12-15 16:24:13 +0800

    finish chapter three

--- object/object.go
+++ object/object.go
@@ -1,14 +1,64 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"monkey_interpreter/ast"
+	"strings"
+)
 
 type ObjectType string
 
 const (
-	IntegerObj = "INTEGER"
-	BooleanObj = "BOOLEAN"
-	NullObj    = "NULL"
+	IntegerObj     = "INTEGER"
+	BooleanObj     = "BOOLEAN"
+	NullObj        = "NULL"
+	ReturnValueObj = "RETURN_VALUE"
+	ErrorObj       = "ERROR"
+	FunctionObj    = "FUNCTION"
 )
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
+func (f *Function) Type() ObjectType {
+	return FunctionObj
+}
+
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	var params []string
+
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ","))
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
+
+type Error struct {
+	Message string
+}
+
+func (e *Error) Type() ObjectType { return ErrorObj }
+func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType { return ReturnValueObj }
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
 
 type Object interface {
 	Type() ObjectType
@@ -49,4 +99,3 @@ func (n *Null) Type() ObjectType {
 func (n *Null) Inspect() string {
 	return "null"
 }
-
